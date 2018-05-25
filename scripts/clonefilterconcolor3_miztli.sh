f75f212b550019e76be8220768483c8a5477d39c
marialadelbarrio <36042187+marialadelbarrio@users.noreply.github.com>
2018-05-24 23:37:36 -0500

    Update and rename clonefilterconcolor3_miztli.sh to scripts/clonefilterconcolor3_miztli.sh

--- scripts/clonefilterconcolor3_miztli.sh
+++ scripts/clonefilterconcolor3_miztli.sh
@@ -11,7 +11,12 @@
 
 module load stacks2/2.0-b8c
 
+#crear el directorio de output para los archivos filtrados
 mkdir /tmpu/pindaro_g/cdm_a/maria/RADcap/filtered
 
+#inputs: son los dos archivos raw comprimidos y se especifica que se cree un archivo log donde se documente el proceso de análisis
+#--inline_inline — random oligo is inline with sequence, occurs on single and paired-end read.
+#-y output type, either 'fastq', 'gzfastq', 'fasta' or 'gzfasta' (default is same as input type).
+#--oligo_len_2 len — length of the paired-end oligo sequence in data set (en este caso 8)
 clone_filter -1 /tmpu/pindaro_g/cdm_a/maria/RADcap/raw/S_concolor_Pool3.1.fq.gz -2 /tmpu/pindaro_g/cdm_a/maria/RADcap/raw/S_concolor_Pool3.2.fq.gz -i gzfastq -o /tmpu/pindaro_g/cdm_a/maria/RADcap/filtered -y gzfastq --inline_inline --oligo_len_2 8 &>> /tmpu/pindaro_g/cdm_a/maria/RADcap/clonefilter.log
 
